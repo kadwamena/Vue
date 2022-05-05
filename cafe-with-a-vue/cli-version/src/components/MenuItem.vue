9266b8b8f5228b6c468a397d4458330f8fe8d8c2
kadwamena <kwabena.dwamena@amalitech.org>
2022-05-05 21:21:43 +0000

    use props in single file components

---
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+    <h3>{{ name }}</h3>
+    <p v-if="inStock">In Stock</p>
+    <p v-else>Out of Stock</p>
+    <label for="add-item-quantity">Quantity: {{ quantity }}</label>
+    <input v-model.number="quantity" id="add-item-quantity" type="number" />
+    <button @click="addToShoppingCart(quantity)">Add to Shopping Cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: ["image", "name", "inStock", "quantity", "addToShoppingCart"],
+};
+</script>
+
+<style>
+</style>
