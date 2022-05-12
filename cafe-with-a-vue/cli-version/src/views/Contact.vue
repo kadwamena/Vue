1fb70d3e1b27589d2224cf8cd213a886e88e562a
kadwamena <kwabena.dwamena@amalitech.org>
2022-05-12 10:39:46 +0000

    manage navigation with vue router

---
+++ cafe-with-a-vue/cli-version/src/views/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contact">
+    <h2>Contact Us</h2>
+    <p>Address: {{ address }}</p>
+    <p>Phone: {{ phone }}</p>
+    <p>Email: {{ email }}</p>
+    <p>Hours:</p>
+    <ul>
+      <li>Monday - Friday: 06:00 to 16:00</li>
+      <li>Saturday: 07:00 to 14:00</li>
+      <li>Sunday: 07:00 to 12:00</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      address: "9274 Beurre Ave, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "+08 88 88 88 88 88",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
