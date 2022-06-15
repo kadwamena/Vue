884a400703b77f6548246371531e3b2d4185037b
kadwamena <kwabena.dwamena@amalitech.org>
2022-06-15 13:25:53 +0000

    retrieve data from vuex

--- cafe-with-a-vue/cli-version/src/store/index.js
+++ cafe-with-a-vue/cli-version/src/store/index.js
@@ -5,6 +5,46 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    shoppingCart: 0,
+    restaurantName: 'Cafe with A Vue',
+    simpleMenu: [
+      {
+        name: 'Crossiant',
+        image: {
+          source: '/images/crossiant.jpg',
+          alt: 'A crossiant'
+        },
+        inStock: true,
+        quantity: 1,
+        price: 2.99
+      },
+      {
+        name: 'French Baguette',
+        image: {
+          source: '/images/french-baguette.jpeg',
+          alt: 'Four French Baguettes'
+        },
+        inStock: true,
+        quantity: 1,
+        price: 3.99
+      },
+      {
+        name: 'Éclair',
+        image: {
+          source: '/images/eclair.jpg',
+          alt: 'Chocolate Éclair'
+        },
+        inStock: false,
+        quantity: 1,
+        price: 4.99
+      }
+    ]
+  },
+  getters : {
+    copyright: state => {
+      const currentYear = new Date().getFullYear()
+      return `Copyright ${state.restaurantName} ${currentYear}`
+    }
   },
   mutations: {
   },
